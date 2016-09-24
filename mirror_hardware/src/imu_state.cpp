73f5b2ef1abb76ebc5b6258ba7a97cdea620290d
Conor Cary <cary.42@osu.edu>
2016-09-23 20:43:05 -0400

    Lint hardware

--- mirror_hardware/src/imu_state.cpp
+++ mirror_hardware/src/imu_state.cpp
@@ -1,3 +1,29 @@
+/*********************************************************************************
+ *  Copyright (c) 2015, The Underwater Robotics Team
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
 
 #include "ros/ros.h"
 #include "message_filters/subscriber.h"
@@ -14,7 +40,7 @@ sensor_msgs::Imu state;
 // tf::StampedTransform tform;
 // tf::Quaternion qturn;
 
-tf::Matrix3x3 rotation = tf::Matrix3x3(1, 0, 0, 0,-1, 0, 0, 0,-1);
+tf::Matrix3x3 rotation = tf::Matrix3x3(1, 0, 0, 0, -1, 0, 0, 0, -1);
 
 void callback(const sensor_msgs::Imu::ConstPtr& imu, const imu_3dm_gx4::FilterOutput::ConstPtr& filter)
 {
@@ -44,13 +70,13 @@ void callback(const sensor_msgs::Imu::ConstPtr& imu, const imu_3dm_gx4::FilterOu
 
   unifier.publish(state);
 
- // tf::quaternionMsgToTF(filter->orientation, qturn);
- // tform.setRotation(qturn);
- // tform.stamp = imu->header.stamp;
- // bcast.sendTransform(tform);
+  // tf::quaternionMsgToTF(filter->orientation, qturn);
+  // tform.setRotation(qturn);
+  // tform.stamp = imu->header.stamp;
+  // bcast.sendTransform(tform);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   ros::init(argc, argv, "imu_state");
 
